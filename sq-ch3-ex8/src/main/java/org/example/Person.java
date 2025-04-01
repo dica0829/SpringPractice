aff01d2ed720483c83ee81a912169b3d692f721d
Keonwoo <dica0829@naver.com>
2025-04-01 15:09:19 +0900

    25-04-01

--- sq-ch3-ex8/src/main/java/org/example/Person.java
+++ sq-ch3-ex8/src/main/java/org/example/Person.java
@@ -21,10 +21,6 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Parrot getParrot() {
         return parrot;
     }
